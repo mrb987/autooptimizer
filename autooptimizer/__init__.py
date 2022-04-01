a7e02a109fb90fac68eb388cb5b5a71d325e7d06
mohammadreza barghi <zaydex_lona@live.com>
2022-04-01 13:05:30 +0430

    Delete autooptimizer directory

--- autooptimizer/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dbscan import dbscan
-from .decisiontree import decisiontree
-from .kmeans import kmeans
-from .knn import knn
-from .logreg import logreg
-from .meanshift import meanshift
-from .svc import svc
